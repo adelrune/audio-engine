78a87e996a6fe3771c26baa76f1b23402215d68f
Guillaume Riou <guiloua2@hotmail.com>
2019-07-24 12:33:22 -0400

    Revert "This compiles but will not ever run... Haaaaalp"
    This reverts commit a9e12331a091f22e490ea2a0005a4b6b1e9fd587.

--- audio-engine/src/audio_objects.rs
+++ audio-engine/src/audio_objects.rs
@@ -8,48 +8,45 @@ fn lin_interpolate(val_1:f32, val_2:f32, location:f32) -> f32{
 }
 
 pub trait Audio {
-    fn next(&mut self) -> ();
-    fn get_sample(&self) -> f32;
+    fn next(&mut self) -> f32;
 }
 
 // Adds the Audio traits to be able to pass numerics to audio objects as parameters.
 impl Audio for f32 {
-    fn next(&mut self) -> () {}
-    fn get_sample(&self) -> f32{
+    fn next(&mut self) -> f32 {
         *self
     }
 }
 
 impl Audio for i32 {
-    fn next(&mut self) -> () {}
-    fn get_sample(&self) -> f32{
+    fn next(&mut self) -> f32 {
         *self as f32
     }
 }
 
 
-pub struct NaiveTableOsc<'a, T:Audio, U:Audio, V:Audio> {
+pub struct NaiveTableOsc<T:Audio, U:Audio, V:Audio> {
     cur_index: f32,
-    pub freq: &'a T,
-    pub amp:&'a U,
-    pub add:&'a V,
+    pub freq: T,
+    pub amp: U,
+    pub add: V,
     pub table: &'static [f32],
     table_increment: f32,
 }
-impl <'a, T:Audio, U:Audio, V:Audio> NaiveTableOsc<'a, T, U, V> {
-    pub fn new(freq:&'a T, amp:&'a U, add:&'a V, table: &'static [f32]) -> Self {
+impl <T:Audio, U:Audio, V:Audio> NaiveTableOsc<T, U, V> {
+    pub fn new(freq: T, amp:U, add:V, table: &'static [f32]) -> NaiveTableOsc<T, U, V> {
         NaiveTableOsc{cur_index:0.0,freq:freq, amp:amp, add:add, table:table, table_increment:table.len() as f32 / SAMPLE_RATE}
     }
 }
 
-impl <'a, T:Audio, U:Audio, V:Audio> Audio for NaiveTableOsc<'a, T, U, V> {
-    fn get_sample(&self) -> f32 {
+impl <T:Audio, U:Audio, V:Audio> Audio for NaiveTableOsc<T, U, V> {
+    fn next(&mut self) -> f32{
         //gets the next samples for freq and mul.
-        let amp = self.amp.get_sample();
-        let add = self.add.get_sample();
+        let amp = self.amp.next();
+        let add = self.add.next();
 
         let fract_part = self.cur_index - self.cur_index.floor();
-        let int_part = self.cur_index as usize;
+        let mut int_part = self.cur_index as usize;
         let int_part = int_part % self.table.len();
         let next = (int_part + 1) % self.table.len();
 
@@ -57,30 +54,28 @@ impl <'a, T:Audio, U:Audio, V:Audio> Audio for NaiveTableOsc<'a, T, U, V> {
             self.table[next],
             fract_part
         );
+
+        let phase_increment = self.freq.next() * self.table_increment;
+        self.cur_index = int_part as f32 + fract_part + phase_increment;
         val * amp + add
     }
-    fn next(&mut self) -> () {
-        let phase_increment = self.freq.get_sample() * self.table_increment;
-        self.cur_index += phase_increment;
-    }
 }
 
 
-pub struct TanHWaveshaper<'a, T:Audio, U:Audio> {
-    input : &'a T,
-    pub drive: &'a U,
+pub struct TanHWaveshaper<T:Audio, U:Audio> {
+    input : T,
+    pub drive: U,
 }
 
-impl <'a, T:Audio, U:Audio> TanHWaveshaper<'a, T, U> {
-    pub fn new(input:&'a T, drive:&'a U) -> Self {
+impl <T:Audio, U:Audio> TanHWaveshaper<T, U> {
+    pub fn new(input: T, drive: U) -> Self{
         TanHWaveshaper{input:input, drive:drive}
     }
 }
 
-impl <'a, T:Audio, U:Audio> Audio for TanHWaveshaper<'a, T, U> {
-    fn get_sample(&self) -> f32 {
-        let drive = self.drive.get_sample();
-        (self.input.get_sample() * drive).tanh()/drive.tanh()
+impl <T:Audio, U:Audio> Audio for TanHWaveshaper<T, U> {
+    fn next(&mut self) -> f32 {
+        let drive = self.drive.next();
+        (self.input.next() * drive).tanh()/drive.tanh()
     }
-    fn next(&mut self) -> () {}
 }
