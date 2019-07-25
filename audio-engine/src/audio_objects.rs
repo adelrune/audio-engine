3d1c0d885bca80032dcf58c6b1ab76711d4bd65f
Vorachose <beland.f@hotmail.ca>
2019-07-25 08:55:32 -0400

    En direction vers une matrice d'etat. Le son a besoin de debugging. C'est plus clair imo par contre

--- audio-engine/src/audio_objects.rs
+++ audio-engine/src/audio_objects.rs
@@ -2,90 +2,44 @@ use libm::F32Ext;
 
 static SAMPLE_RATE : f32 = 44100.0;
 
-pub struct ValueChan<T> {
-    value: *const T
-}
-
-impl<T> ValueChan<T> {
-    pub fn from(from: *const T) -> ValueChan<T> {
-        ValueChan {
-            value: from
-        }
-    }
-
-    pub fn get(&self) -> &T {
-        unsafe {self.value.as_ref().unwrap()}
-    }
-}
-
-
-//not sure interpolate should be there, will probably move it later
 fn lin_interpolate(val_1:f32, val_2:f32, location:f32) -> f32{
     val_1 * (1.0 - location) + val_2 * location
 }
 
-// Adds the Audio traits to be able to pass numerics to audio objects as parameters.
-pub struct ConstantOsc {
-    value: f32
-}
-
-impl ConstantOsc {
-    pub fn get_value_chan(&self) -> ValueChan<f32> {
-        ValueChan::from(&self.value)
-    }
-}
-
-impl From<i32> for ConstantOsc {
-    fn from(value: i32) -> ConstantOsc {
-        ConstantOsc {
-            value: value as f32
-        }
-    }
-}
-
-impl From<f32> for ConstantOsc {
-    fn from(value: f32) -> ConstantOsc {
-        ConstantOsc {
-            value
-        }
-    }
+pub trait AudioComponent<T: Copy, V> {
+    fn initial_state(&self) -> V;
+    fn step(&mut self, input: T);
+    fn sample(&self, input: T) -> V;
+    fn step_and_sample(&mut self, input: T) -> V { self.step(input); self.sample(input) }
 }
 
 pub struct NaiveTableOsc {
     cur_index: f32,
-    pub freq: ValueChan<f32>,
-    pub amp: ValueChan<f32>,
-    pub add: ValueChan<f32>,
-    pub table: &'static [f32],
-    table_increment: f32,
-    current_value: f32
+    table: &'static [f32],
+    table_increment: f32
 }
 
 impl NaiveTableOsc {
-    pub fn new(freq: ValueChan<f32>, amp: ValueChan<f32>, add: ValueChan<f32>, table: &'static [f32]) -> Self {
+    pub fn new(table: &'static [f32]) -> NaiveTableOsc {
         NaiveTableOsc {
             cur_index: 0.0,
-            freq, 
-            amp, 
-            add, 
-            table, 
-            table_increment: table.len() as f32 / SAMPLE_RATE,
-            current_value: 0.0
+            table,
+            table_increment: table.len() as f32 / SAMPLE_RATE
         }
     }
+}
 
-    pub fn get_value_chan(&self) -> ValueChan<f32> {
-        ValueChan::from(&self.current_value)
+impl AudioComponent<(f32, f32, f32), f32> for NaiveTableOsc {
+    fn initial_state(&self) -> f32 { 0.0 }
+
+    fn step(&mut self, input: (f32, f32, f32)) {
+        let phase_increment = input.0 * self.table_increment;
+        self.cur_index += phase_increment;
     }
 
-    pub fn next(&mut self) {
-        let freq = self.freq.get();
-        let phase_increment = freq * self.table_increment;
-        self.cur_index += phase_increment;
-
-            //gets the next samples for freq and mul.
-        let amp = self.amp.get();
-        let add = self.add.get();
+    fn sample(&self, input: (f32, f32, f32)) -> f32 {
+        let amp = input.1;
+        let add = input.2;
 
         let fract_part = self.cur_index - self.cur_index.floor();
         let int_part = self.cur_index as usize;
@@ -96,32 +50,26 @@ impl NaiveTableOsc {
             self.table[next],
             fract_part
         );
-        self.current_value = val * amp + add;
+        val * amp + add
     }
 }
 
-
 pub struct TanHWaveshaper {
-    input : ValueChan<f32>,
-    pub drive: ValueChan<f32>,
-    current_value: f32
 }
 
 impl TanHWaveshaper {
-    pub fn new(input: ValueChan<f32>, drive: ValueChan<f32>) -> Self {
-        TanHWaveshaper {
-            input, 
-            drive,
-            current_value: 0.0
-        }
+    pub fn new() -> Self {
+        TanHWaveshaper {}
     }
+}
 
-    pub fn get_value_chan(&self) -> ValueChan<f32> {
-        ValueChan::from(&self.current_value)
-    }
+impl AudioComponent<(f32, f32), f32> for TanHWaveshaper {
+    fn initial_state(&self) -> f32 { 0.0 }
 
-    pub fn next(&mut self) {
-        let drive = self.drive.get();
-        self.current_value = (self.input.get() * drive).tanh()/drive.tanh();
+    fn step(&mut self, input: (f32, f32)) {}
+
+    fn sample(&self, input: (f32, f32)) -> f32 {
+        let drive = input.0;
+        (input.1 * drive).tanh()/drive.tanh()
     }
 }
--- tests/src/main.rs
+++ tests/src/main.rs
@@ -2,8 +2,8 @@ extern crate portaudio;
 
 use portaudio as pa;
 use audio_engine::audio_objects::{
+    AudioComponent,
     NaiveTableOsc,
-    ConstantOsc,
     TanHWaveshaper
 };
 use audio_engine::tables::{SINE_2048, TRIANGLE_2};
@@ -26,40 +26,38 @@ fn run() -> Result<(), pa::Error> {
         "PortAudio Test: output sine wave. SR = {}, BufSize = {}",
         SAMPLE_RATE, FRAMES_PER_BUFFER
     );
-    let stable_0_3 = ConstantOsc::from(0.3);
-    let stable_300 = ConstantOsc::from(300);
-    let stable_660 = ConstantOsc::from(660);
-    let stable_220 = ConstantOsc::from(220);
-    let stable_440 = ConstantOsc::from(440);
-    let stable_1 = ConstantOsc::from(1);
-    let stable_0 = ConstantOsc::from(0);
-    let stable_2_3 = ConstantOsc::from(2.3);
-    let stable_3 = ConstantOsc::from(3);
-    let stable_3_2 = ConstantOsc::from(3.2);
 
+    let mut modmod = NaiveTableOsc::new(&SINE_2048);
+    let mut modulator = NaiveTableOsc::new(&SINE_2048);
+    let mut sine_osc = NaiveTableOsc::new(&SINE_2048);
+    let mut disto_mod = NaiveTableOsc::new(&TRIANGLE_2);
+    let mut output = TanHWaveshaper::new();
 
-    let mut modmod = NaiveTableOsc::new(stable_0_3.get_value_chan(), stable_300.get_value_chan(), stable_660.get_value_chan(), &SINE_2048);
-    let mut modulator = NaiveTableOsc::new(modmod.get_value_chan(), stable_220.get_value_chan(), stable_440.get_value_chan(), &SINE_2048);
-    let mut sine_osc = NaiveTableOsc::new(modulator.get_value_chan(), stable_1.get_value_chan(), stable_0.get_value_chan(), &SINE_2048);
-    let mut disto_mod = NaiveTableOsc::new(stable_2_3.get_value_chan(), stable_3.get_value_chan(), stable_3_2.get_value_chan(), &TRIANGLE_2);
-    let mut last = TanHWaveshaper::new(sine_osc.get_value_chan(), disto_mod.get_value_chan());
-    // let mut last = audio_objects::NaiveTableOsc::new(440,1, 0, &TRIANGLE_2);
+    let mut state = [
+        modmod.initial_state(),
+        modulator.initial_state(),
+        sine_osc.initial_state(),
+        disto_mod.initial_state(),
+        output.initial_state()
+    ];
+
     let pa = pa::PortAudio::new()?;
     let settings =
         pa.default_output_stream_settings(CHANNELS, SAMPLE_RATE, FRAMES_PER_BUFFER)?;
 
 
     let callback = move |pa::OutputStreamCallbackArgs { buffer, frames, .. }| {
-        let last_chan = last.get_value_chan();
         let mut idx = 0;
         for _ in 0..frames {
-            let samp = *last_chan.get();
-    
-            modmod.next();
-            modulator.next();
-            sine_osc.next();
-            disto_mod.next();
-            last.next();
+            let samp = state[4];
+
+            let old_state = state.clone();
+
+            state[0] = modmod.step_and_sample((0.3, 300.0, 660.0));
+            state[1] = modulator.step_and_sample((old_state[0], 220.0, 440.0));
+            state[2] = sine_osc.step_and_sample((old_state[1], 1.0, 0.0));
+            state[3] = disto_mod.step_and_sample((2.3, 3.0, 3.2));
+            state[4] = output.step_and_sample((old_state[2], old_state[3]));
 
             buffer[idx] = samp;
             buffer[idx + 1] = samp;
